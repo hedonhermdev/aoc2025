9622fe0172d33488a6501f6ae8e2587caf8af67e
Tirth Jain <jaintirth24@gmail.com>
2025-12-09 00:14:01 +0530

    some optimization for day 8

--- src/solution/day8.rs
+++ src/solution/day8.rs
@@ -1,13 +1,15 @@
-use core::num;
 use itertools::Itertools;
-use std::collections::{BinaryHeap, HashMap, HashSet};
+use std::collections::{HashMap, HashSet};
 
 use aoc_runner_derive::*;
 
-#[derive(PartialEq, Eq, Hash)]
+#[derive(PartialEq, Eq, Hash, PartialOrd, Ord)]
 struct JunctionBox(u64, u64, u64);
 
 type Input = Vec<JunctionBox>;
+
+#[cfg(not(test))]
+const NUM_CONNECTIONS: usize = 1000;
 
 struct DSU<'a> {
     parent: HashMap<&'a JunctionBox, &'a JunctionBox>,
@@ -88,21 +90,18 @@ fn parse_input(input: &str) -> Input {
         .collect()
 }
 
-struct DistPair<'a> {
-    j1: &'a JunctionBox,
-    j2: &'a JunctionBox,
-    dist: f64,
-}
+#[derive(Eq, PartialEq, PartialOrd, Ord)]
+struct DistPair<'a>(u128, &'a JunctionBox, &'a JunctionBox);
 
 impl<'a> DistPair<'a> {
     fn new(j1: &'a JunctionBox, j2: &'a JunctionBox) -> Self {
-        let dx = (j1.0 as f64 - j2.0 as f64).powi(2) as f64;
-        let dy = (j1.1 as f64 - j2.1 as f64).powi(2) as f64;
-        let dz = (j1.2 as f64 - j2.2 as f64).powi(2) as f64;
+        let dx = (j1.0 as i128 - j2.0 as i128).pow(2) as u128;
+        let dy = (j1.1 as i128 - j2.1 as i128).pow(2) as u128;
+        let dz = (j1.2 as i128 - j2.2 as i128).pow(2) as u128;
 
         let dist = dx + dy + dz;
 
-        Self { j1, j2, dist }
+        Self(dist, j1, j2)
     }
 
     fn from_arr(arr: [&'a JunctionBox; 2]) -> Self {
@@ -110,64 +109,34 @@ impl<'a> DistPair<'a> {
     }
 
     fn pair(&self) -> (&'a JunctionBox, &'a JunctionBox) {
-        (self.j1, self.j2)
-    }
-}
-
-impl<'a> PartialEq for DistPair<'a> {
-    fn eq(&self, other: &Self) -> bool {
-        self.dist == other.dist
-    }
-}
-
-impl<'a> Eq for DistPair<'a> {}
-
-impl<'a> PartialOrd for DistPair<'a> {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        self.dist.partial_cmp(&other.dist)
-    }
-}
-
-impl<'a> Ord for DistPair<'a> {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.partial_cmp(other).unwrap()
+        (self.1, self.2)
     }
 }
 
 #[aoc(day8, part1)]
 fn puzzle1(input: &Input) -> u64 {
-    let mut heap = BinaryHeap::new();
-
-    for [j1, j2] in input.iter().array_combinations() {
-        heap.push(DistPair::new(j1, j2));
-
-        if heap.len() > 1000 {
-            heap.pop();
-        }
-    }
-
     let mut dsu = DSU::new(input);
-    for dp in heap {
+
+    input.iter().array_combinations().map(|arr| DistPair::from_arr(arr)).k_largest(NUM_CONNECTIONS).for_each(|dp| {
         let (j1, j2) = dp.pair();
-        dsu.union(j1, j2);
-    }
+        dsu.union(j1, j2);        
+    });
 
     dsu.roots()
         .map(|root| dsu.size[root] as u64)
-        .sorted()
-        .rev()
-        .take(3)
+        .k_largest(3)
         .product()
 }
 
 #[aoc(day8, part2)]
 fn puzzle2(input: &Input) -> u64 {
     let mut dsu = DSU::new(input);
+
     for (j1, j2) in input
         .iter()
         .array_combinations()
         .map(DistPair::from_arr)
-        .sorted()
+        .sorted_unstable()
         .map(|dp| dp.pair())
     {
         dsu.union(j1, j2);
@@ -178,3 +147,32 @@ fn puzzle2(input: &Input) -> u64 {
 
     unreachable!()
 }
+
+#[cfg(test)]
+const NUM_CONNECTIONS: usize = 10;
+
+#[cfg(test)]
+const TEST_INPUT: &str = r#"
+162,817,812
+57,618,57
+906,360,560
+592,479,940
+352,342,300
+466,668,158
+542,29,236
+431,825,988
+739,650,466
+52,470,668
+216,146,977
+819,987,18
+117,168,530
+805,96,715
+346,949,466
+970,615,88
+941,993,340
+862,61,35
+984,92,344
+425,690,689
+"#;
+
+crate::aoc_test!(TEST_INPUT, 40, 25272);
